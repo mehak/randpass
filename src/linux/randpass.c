f0a3d4ad5a1ca6772d54063051b8a17a6ad9b799
Nathanael Merlin <thantos@linuxmail.org>
2011-04-13 23:09:27 -0700

    renamed the manpage.  I just realized I have to rename it again. 	new file:   manpage.1 	modified:   src/linux/randpass.c

--- src/linux/randpass.c
+++ src/linux/randpass.c
@@ -1,4 +1,3 @@
-/*
 /*  Copyright 2010 Nathanael Merlin
 
     This file is part of randpass.
@@ -16,6 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+/*
   Name: randpass
   Author: Nate Merlin
   Purpose: To make secure random passwords
